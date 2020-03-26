f63547851c22cca58ac01c6cae74a85e1a655064
pavelalekseev <pavel.alekseev@betica.com>
2020-03-26 10:04:36 +0800

    HW-1: multiThreadMultiply optimization

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,7 +1,9 @@
 package ru.javaops.masterjava.matrix;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -10,40 +12,82 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
-    // TODO implement parallel multiplication matrixA*matrixB
-    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+    private static Random rn = new Random();
+
+    private MatrixUtil() {
+    }
+
+    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException {
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
+        final int[][] matrixBT = getTMatrix(matrixB, matrixSize);
+
+        List<Callable<Boolean>> result = new ArrayList<>(matrixSize);
+
+        for (int i = 0; i < matrixSize; i++) {
+            result.add(new GroupResult(i, matrixSize, matrixA, matrixBT, matrixC));
+        }
+
+        executor.invokeAll(result);
 
         return matrixC;
     }
 
+    public static class GroupResult implements Callable<Boolean> {
+        int i;
+        final int matrixSize;
+        int[][] matrixA;
+        int[][] matrixBT;
+        final int[][] matrixC;
+
+        public GroupResult(int i, int matrixSize, int[][] matrixA, int[][] matrixBT, int[][] matrixC) {
+            this.i = i;
+            this.matrixSize = matrixSize;
+            this.matrixA = matrixA;
+            this.matrixBT = matrixBT;
+            this.matrixC = matrixC;
+        }
+
+        @Override
+        public Boolean call() {
+            mainLoop(i, matrixA[i], matrixBT, matrixC, matrixSize);
+            return true;
+        }
+    }
+
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
         final int matrixSize = matrixA.length;
-        final int[][] matrixBT = new int[matrixSize][matrixSize];
         final int[][] matrixC = new int[matrixSize][matrixSize];
+        final int[][] matrixBT = getTMatrix(matrixB, matrixSize);
 
+        for (int i = 0; i < matrixSize; i++) {
+            mainLoop(i, matrixA[i], matrixBT, matrixC, matrixSize);
+        }
+        return matrixC;
+    }
+
+    private static int[][] getTMatrix(int[][] matrixB, int matrixSize) {
+        int[][] matrixBT = new int[matrixSize][matrixSize];
         for (int i = 0; i < matrixSize; i++) {
             for (int j = 0; j < matrixSize; j++) {
                 matrixBT[j][i] = matrixB[i][j];
             }
         }
+        return matrixBT;
+    }
 
-        for (int i = 0; i < matrixSize; i++) {
-            for (int j = 0; j < matrixSize; j++) {
-                int sum = 0;
-                for (int k = 0; k < matrixSize; k++) {
-                    sum += matrixA[i][k] * matrixBT[j][k];
-                }
-                matrixC[i][j] = sum;
+    private static void mainLoop(int i, int[] rowA, int[][] matrixBT, int[][] matrixC, int matrixSize) {
+        for (int j = 0; j < matrixSize; j++) {
+            int sum = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                sum += rowA[k] * matrixBT[j][k];
             }
+            matrixC[i][j] = sum;
         }
-        return matrixC;
     }
 
     public static int[][] create(int size) {
         int[][] matrix = new int[size][size];
-        Random rn = new Random();
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
