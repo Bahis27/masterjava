bda19a1d9e3092c7d581c55e02f269b9c9cad586
pavelalekseev <pavel.alekseev@betica.com>
2020-05-06 08:37:15 +0800

    patch 8.7: add sh

---
+++ sql/lb_apply.sh
@@ -0,0 +1,2 @@
+export LB_HOME=/Users/pavelalekseev/_dev/liquibase-3.8.9
+sh $LB_HOME/liquibase --driver=org.postgresql.Driver --classpath=$LB_HOME/lib --changeLogFile=databaseChangeLog.sql --url="jdbc:postgresql://localhost:5432/masterjava" --username=user --password=password update
